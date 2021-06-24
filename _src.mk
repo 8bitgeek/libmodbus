5903c0b0ce0906ce97a1662860a59fd8a242e2ce
8bitgeek <mike@8bitgeek.net>
2021-06-24 02:46:04 -0400

    included cook makefile snippet

---
+++ _src.mk
@@ -0,0 +1,9 @@
+# ** LIBMODBUS
+# SRC_LIBMODBUS=${ENV_LIBMODBUS_ROOT}
+SRC_LIBMODBUS=${SRC_ROOT}/libmodbus
+INC += 	-I $(SRC_LIBMODBUS)
+
+SRCS_CC += $(SRC_LIBMODBUS)/modbus.c
+SRCS_CC += $(SRC_LIBMODBUS)/modbus-data.c
+SRCS_CC += $(SRC_LIBMODBUS)/modbus-rtu-caribou.c
+# SRCS_CC += $(SRC_LIBMODBUS)/modbus-tcp-caribou.c
